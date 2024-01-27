83c503ec76ededf6e84cd3b1f9326e0ce272921a
Dylan Beattie <dylan@dylanbeattie.net>
2024-01-27 10:56:11 +0000

    dotnet new mvc

---
+++ Rockaway/Rockaway.WebApp/Models/TicketTypeViewData.cs
@@ -0,0 +1,16 @@
+﻿using Rockaway.WebApp.Data.Entities;
+
+namespace Rockaway.WebApp.Models;
+
+public class TicketTypeViewData {
+
+	public static TicketTypeViewData FromTicketType(TicketType tt) => new() {
+		Name = tt.Name,
+		Price = tt.Price,
+		Id = tt.Id
+	};
+
+	public string Name { get; private set; };
+	public decimal Price { get; private set; };
+	public Guid Id { get; private set; };
+}
