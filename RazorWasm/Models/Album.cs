b1a90875c034dba193206767403550eefa621a2e
Dylan Beattie <dylan@dylanbeattie.net>
2024-01-27 11:37:00 +0000

    Workign with SSR - now let's see if WASM will work...

---
+++ RazorWasm/Models/Album.cs
@@ -0,0 +1,4 @@
+public class Album(string title, string artist) {
+	public string Title { get; set; } = title;
+	public string Artist { get; set; } = artist;
+}
---
+++ RazorWasm/Program.cs
@@ -0,0 +1,18 @@
+using RazorWasm.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+builder.Services.AddRazorComponents().AddInteractiveServerComponents();
+
+var app = builder.Build();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapRazorPages();
+app.MapRazorComponents<App>().AddInteractiveServerRenderMode();
+app.Run();
